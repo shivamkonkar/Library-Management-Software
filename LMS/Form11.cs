5f5f2fab342946aae41bfc231de31c5025775df1
shivamkonkar <shivamkonkar25@gmail.com>
2021-11-28 07:46:05 +0530

    database added

---
+++ LMS/Form11.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess;
+using Oracle.ManagedDataAccess.Client;
+
+namespace LMS
+{
+    public partial class Form11 : Form
+    {
+        public Form11()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form11_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+                string oradb = "Data Source=localhost:1521/XE;User Id = ADMIN1; Password = 08092002; ";
+                OracleConnection conn = new OracleConnection(oradb);
+                conn.Open();
+                OracleCommand cmd = new OracleCommand();
+
+                cmd.CommandText = "insert into REGISTRATION (rid,username,password) values('"+ txtID.Text + "','" + textuser.Text + "','" + textpass.Text + "')";
+
+                cmd.Connection = conn;
+
+                int rowsUpdated = cmd.ExecuteNonQuery();
+                if (rowsUpdated == 0)
+                {
+                    MessageBox.Show("Record not inserted");
+                }
+                else
+                {
+                    MessageBox.Show("Success!");
+                    this.Close();
+                    Form1 login = new Form1();
+                    login.Show();
+                }
+                conn.Dispose();
+            
+        }
+    }
+}
---
+++ LMS/Form8.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace LMS
+{
+    partial class Form8
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LMS.Properties.Resources._6_books_png_image_with_transparency_background;
+            this.pictureBox1.Location = new System.Drawing.Point(162, 118);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(366, 251);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form8
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(765, 477);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form8";
+            this.Text = "Form8";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
